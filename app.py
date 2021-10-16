ca2ea42d7eba3ae73a3acfd9636be13caf656da9
Paul Trayers <ptrayers@gmail.com>
2021-10-16 18:11:03 +0100

    Changed to guessing game

--- app.py
+++ app.py
@@ -1,47 +1,39 @@
 from pywebio.input import *
 from pywebio.output import *
-
+import random
 
 def main():
-    """BMI Calculation
-
-    Simple application for calculating Body Mass Index.
-    """
-
-    put_markdown("""# Body Mass Index
     
-    [Body mass index](https://en.wikipedia.org/wiki/Body_mass_index) (BMI) is a measure of body fat based on height and weight that applies to adult men and women. 
-    
-    BMI Categories:
-    
-    | Category             | BMI           |
-    | -------------------- | ------------- |
-    | Severely underweight | BMI<14.9      |
-    | Underweight          | 14.9≤BMI<18.4 |
-    | Normal               | 18.4≤BMI<22.9 |
-    | Overweight           | 22.9≤BMI<27.5 |
-    | Moderately obese     | 27.5≤BMI<40   |
-    | Severely obese       | BMI≥40        |
-    
-    ## PT's BMI calculator
-    """, strip_indent=4)
+    # Guess the number game
 
-    info = input_group('BMI calculation', [
-        input("Your Height(cm)", name="height", type=FLOAT),
-        input("Your Weight(kg)", name="weight", type=FLOAT),
-    ])
+    guessesTaken = 0
 
-    BMI = info['weight'] / (info['height'] / 100) ** 2
+    put_markdown('Hello! What is your name?')
+    myName = input()
 
-    top_status = [(14.9, 'Severely underweight'), (18.4, 'Underweight'),
-                  (22.9, 'Normal'), (27.5, 'Overweight'),
-                  (40.0, 'Moderately obese'), (float('inf'), 'Severely obese')]
+    number = random.randint(1, 20)
+    put_markdown('Ok `%s`, I\'m thinking of number between 1 and `%.1f`', % (myName, number))
 
-    for top, status in top_status:
-        if BMI <= top:
-            put_markdown('Your BMI: `%.1f`, Category: `%s`' % (BMI, status))
+    for guessesTaken in range(6):
+        #put_markdown('Take a guess.') # 4 spaces to indent
+        guess = input("Take a guess.", type=FLOAT)
+        #guess = int(guess)
+        if guess < number:
+            put_markdown('Your guess is too low.') # Eight spaces indent
+        if guess > number:
+            put_markdown('Your guess is too high.')
+
+        if guess == number:
             break
 
+    if guess == number:
+        guessesTaken = str(guessesTaken + 1)
+        put_markdown('Good job, `%s`! You guessed number in `%.1f` guesses!' % (myName, guessesTaken))
+
+    if guess != number:
+        number = str(number)
+        print('Bad luck! The number I was thinking of was `%.1f`.' % (number))
+    
 
 if __name__ == '__main__':
     import argparse

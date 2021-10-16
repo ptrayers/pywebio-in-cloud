8fb803ed3a1831a6af0c4dea4e696e96ff48af9f
Paul Trayers <ptrayers@gmail.com>
2021-10-16 20:57:26 +0100

    Add button to try again, catch invalid non-numeric input

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import pywebio
 import random
-from pywebio.input import input, FLOAT, NUMBER
-from pywebio.output import put_text, put_html, put_markdown, put_table
+from pywebio.input import *
+from pywebio.output import put_text, put_html, put_markdown, put_table, put_buttons
+from pywebio.session import *
 
+op = {}
+op['Try Again'] = 'window.location.reload()'
+op['Close window'] = 'window.close()'
+
+def tab_operation(choice):
+    run_js(op[choice])
+    
 def guess_num():
     # Guess the number game
 
@@ -24,7 +32,12 @@ def guess_num():
                 put_markdown('That\'s not a number %s!' % myName)
                 continue
         
-            #guess = int(guess)
+            try:
+                guess = int(guess)
+            except:
+                put_markdown('That\'s not a number %s!' % myName)
+                continue
+            
             if guess < number:
                 put_markdown('Your guess is too low.') # Eight spaces indent
             if guess > number:
@@ -40,7 +53,8 @@ def guess_num():
         if guess != number:
             put_text('Bad luck! The number I was thinking of was %0.f.' % (number))
             
-        confirm = actions('Try again?', ['Yes'])
+        put_buttons(op.keys(), onclick=tab_operation)
+        hold()
     
 
 if __name__ == '__main__':

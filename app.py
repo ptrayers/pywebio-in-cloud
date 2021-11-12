582cf1cd0d9ac159d3f2c648edcb553c6def7c64
Paul Trayers <ptrayers@gmail.com>
2021-11-12 20:59:07 +0000

    8 times tables

--- app.py
+++ app.py
@@ -87,73 +87,33 @@ def guess_num():
     else:
         put_text('Hard luck! The number I was thinking of was %0.f. %s' % (number, "\U0001F62D"))
 
-
 def times_tables():
     # times tables
 
-    times = 8
-    maxNum = 12
-    maxGuesses = 10
-    guessesTaken = 0
+    maxQuestions = 10
+    times = 8  # x time tables
+    maxNum = 12  # max number the user is asked to multiply times with 
 
     #put_markdown('Hello! What is your name?')
-    myName = input("Hello! What is your name?")
+    myName = "Adam" # input("Hello! What is your name?")
 
     #while True:
-    number = random.randint(1, maxNum)
-    answer = times * number
-    put_markdown('Ok %s, What is %.0f x %.0f' % (myName, times, number))
-
-    for guessesTaken in range(0, maxGuesses):
-
-        guessesLeft = (maxGuesses - guessesTaken - 1)
- 
-        inc = 0.10
-        percentGuessesLeft = guessesLeft / maxGuesses
-        status_emojis = [(1.0, ''),
-                         (inc*6, '\U0001F92D'), #\N{face with hand over mouth}'),
-                         (inc*5, '\N{thinking face}'), 
-                         (inc*4, '\U0001F92B'), #\N{shushing face}'),
-                         (inc*3, '\U0001F633'), #\N{⊛ face with open eyes and hand over mouth}'),
-                         (inc*2, '\U0001F62C'),
-                         (inc, '\U0001F631')] #\N{⊛ face with peeking eye}')]
-                         #(float('inf'), 'Severely obese')]
-
-        emoj = ''
-        for guesses, emoj in status_emojis:
-            if percentGuessesLeft <= guesses:
-                break  # emoj has been set by loop ;)
-        extraMsgs = [
-            '.', '.', '.', '.',
-            '.', '.', '.', '.',
-            '.', '.'
-            ]
-        statusMsg = 'You have %s tries left%s %s' % (guessesLeft, random.choice(extraMsgs), emoj)
+    for questions in range(0, maxQuestions):
+        number = random.randint(1, maxNum)
+        answer = times * number
 
         #put_markdown('Take a guess.') # 4 spaces to indent
         try:
-            guess = input("What is the answer?", type=NUMBER)
-            guess = int(guess)
+            userAnswer = input ('Ok %s, What is %.0f x %.0f' % (myName, times, number), type=NUMBER)
+            userAnswer = int(userAnswer)
         except:    
-            put_text('That\'s not a number %s! %s' % (myName, statusMsg))
+            put_text('That\'s not a number %s!' % (myName))
             continue
 
-            
-        if guess == answer:
-            break
-        elif guess < answer:
-            put_markdown('Your guess is too low. ' + statusMsg)
-        elif guess > answer:
-            put_markdown('Your guess is too high. ' + statusMsg)
-
-    if guess == answer:
-        guessesTaken = str(guessesTaken + 1)
-        put_text('Good job, %s! You got it in %s guesses! %s' % (myName, guessesTaken, "\U0001F60A \U0001F64C"))
-        put_text('\U0001F64C').style('font-size: 144px')
-        #put_text('Good job, %s! You guessed Daddy\'s age today! %s' % (myName, "\U0001F60A \U0001F64C"))
-        #put_text('\U0001F388 \U00000035 \U00000030 \U0001F389').style('font-size: 144px')
-    else:
-        put_text('Hard luck! The number is %0.f. %s' % (answer, "\U0001F62D"))
+        if userAnswer == answer:
+            put_text('Correct')
+        else:
+            put_text('Incorrect')
 
 
 if __name__ == '__main__':
@@ -167,9 +127,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.http:
-        start_http_server(guess_num, port=args.port)
+        start_http_server(times_tables, port=args.port)
     else:
         # Since some cloud server may close idle connections (such as heroku),
         # use `websocket_ping_interval` to  keep the connection alive
-        start_ws_server(guess_num, port=args.port, websocket_ping_interval=30)
+        start_ws_server(times_tables, port=args.port, websocket_ping_interval=30)
 
